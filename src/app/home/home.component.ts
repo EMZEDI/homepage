a1ad6b0022b03154b4e945652af108dd6d8161c5
Shahrad <shahrad_m@icloud.com>
2024-10-29 15:49:06 -0400

    site update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,22 +14,27 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent implements OnInit {
   htmlContents = [
-    'This is <span class="bold bg-gray-700 rounded-md text-violet-500 hover:text-violet-300">Shahrad</span> :) Welcome to my website!',
-    '<div class="bg-cyan-300/25 rounded-2xl text-green-300 hover:text-green-700 text-center max-w-sm m-auto text-xl mt-8"><span class="underline decoration-blue-500 text-pink-400/75">print("</span><span class="hover:bg-blue-700/25 rounded-2xl">I am a Machine Learning developer, researcher, and a prev. Intern at Morgan Stanley.</span> <span class="underline decoration-blue-500 text-pink-400/75">")</span> </div>',
-    '<div class="bg-cyan-300/25 rounded-2xl text-green-300 hover:text-green-700 text-center max-w-lg m-auto text-xl"><span class="underline decoration-blue-500 text-pink-400/75"> system.out.println("</span><span class="hover:bg-blue-700/25 rounded-2xl" > + oh also looking for a summer 2024 internship in Machine Learning and Data Science. You can safely scroll!</span><span class="underline decoration-blue-500 text-pink-400/75">")</span></div>',
+    '<div class="mt-14">This is <span class="bold bg-gray-700 rounded-md text-violet-500 hover:text-violet-600">Shahrad</span> :) Welcome!</div>',
+    '<div class="bg-cyan-300/25 rounded-2xl text-green-300 hover:text-green-700 text-center max-w-sm m-auto text-xl mt-8"><span class="underline decoration-blue-500 text-pink-400/75">print("</span><span class="hover:bg-blue-700/25 rounded-2xl">I am a Machine Learning Researcher and Master\'s student at Mila/McGill with Doina Precup and Reihaneh Rabbany.</span> <span class="underline decoration-blue-500 text-pink-400/75">")</span> </div>',
   ];
   projects = [
     {
-      image: '../../assets/pictures/moritz-kindler-G66K_ERZRhM-unsplash.jpg',
-      name: 'Temporal Collaborative Filtering for Link Prediction in Temporal Graphs',
-      description: 'I improved Edgebank for link prediction in temporal graphs by introducing a novel metric, sigma (σ), measuring temporal dynamics. Using PyTorch and SciPy, I implemented a temporal collaborative filtering algorithm capturing both temporal and edge-level dynamics. The paper is under review for the ICLR workshop in February 2024.',
-      githubLink: 'https://github.com/EMZEDI/TGB_baseline/settings'
+      image: '../../assets/pictures/istockphoto-1486380350-612x612.jpg',
+      name: 'Hallucination Detox: Sensitive Neuron Dropout (SeND) for Large Language Model Training @ NeurIPS 2024 SafeGenAI',
+      description: 'Our research focuses on improving the reliability of large language models (LLMs) by addressing hallucinations directly during training, rather than relying on post hoc fixes. We introduce Sensitive Neuron Dropout (SeND) and the Efficient EigenScore (EES) metric, which together reduce hallucinations and improve model accuracy by targeting neuron variability and enhancing scalability.',
+      githubLink: 'https://arxiv.org/abs/2410.15460'
     },
     {
       image: '../../assets/pictures/istockphoto-1486380350-612x612.jpg',
-      name: 'Distillation of Transformers with Efficient Mechanisms',
-      description: 'We distilled decoder-only transformers like GPT-3 by employing cross-architectural distillation, replacing attention heads with an efficient Hyena mechanism. This led to a remarkable reduction in training time and memory usage, achieving a perplexity score over 40% better than the pre-trained Hyena model. Supervised by Dr. Massaroli from Yoshua Bengio\'s group, the project utilized PyTorch for multi-GPU training and various tools like HuggingFace and Weights and Biases. Currently, the findings are under review for presentation at the ICML 2024 conference.',
-      githubLink: 'javascript:void(0)'
+      name: 'Distillation of Transformers with Efficient Mechanisms @ ICML 2024',
+      description: 'Winner of UofT AI Project X 2024 Competition. We distilled decoder-only transformers like GPT-3 by employing cross-architectural distillation, replacing attention heads with an efficient Hyena mechanism. This led to a remarkable reduction in training time and memory usage, achieving a perplexity score over 40% better than the pre-trained Hyena model. Supervised by Dr. Massaroli from Yoshua Bengio\'s group, the project utilized PyTorch for multi-GPU training and various tools like HuggingFace and Weights and Biases. Accepted to ICML 2024 ES FOMO II Workshop.',
+      githubLink: 'https://arxiv.org/pdf/2401.17574'
+    },
+    {
+      image: '../../assets/pictures/moritz-kindler-G66K_ERZRhM-unsplash.jpg',
+      name: 'Temporal Collaborative Filtering for Link Prediction in Temporal Graphs',
+      description: 'I improved Edgebank for link prediction in temporal graphs by introducing a novel metric, sigma (σ), measuring temporal dynamics. Using PyTorch and SciPy, I implemented a temporal collaborative filtering algorithm capturing both temporal and edge-level dynamics.',
+      githubLink: 'https://drive.google.com/file/d/1sS2vF2qAE_zP744WUqroEjqHeI7K9Jbd/view'
     },
     {
       image: '../../assets/pictures/360_F_336903647_0HD6RdVkAdiY9qp7ZCf150lMd2lHKZXB.jpg',
@@ -56,7 +61,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     let i = 0;
     let contentIndex = 0;
-    let intervalTime = 100; // initial interval time
+    let intervalTime = 50; // initial interval time
 
     const typeContent = () => {
       if (this.htmlContents[contentIndex][i] === '<') {
@@ -72,7 +77,7 @@ export class HomeComponent implements OnInit {
         contentIndex++;
         // If the next content is one of the middle two, decrease the interval time
         if (contentIndex === Math.floor(this.htmlContents.length / 2) || contentIndex === Math.floor(this.htmlContents.length / 2) - 1) {
-          intervalTime = 50; // adjust this value as needed
+          intervalTime = 35; // adjust this value as needed
         } else {
           intervalTime = 100; // reset the interval time for other elements
         }
